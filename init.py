18f4d59a6a59cc09615fccdb1e8bc9518588033e
EvolueDEVs <programacaoevolue@gmail.com>
2022-06-02 08:54:35 -0300

    Atualização de desempenho

--- init.py
+++ init.py
@@ -2,4 +2,3 @@ import files.verifyFolders as dskTv
 import confing.cof as conf
 
 deskTopVerify = dskTv.VerifyFolders(conf.confing["foldersForVerirify"])
-# print(deskTopVerify.listFiles(conf.confing["foldersForVerirify"]))
