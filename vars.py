851f0f1f945e1f786db6d3f3322dd0bb1062b090
RenzoNogueira <mainrenzossn@gmail.com>
2022-06-02 22:01:03 -0300

    Caminhos relativos adicionado ás variáveis

--- vars.py
+++ vars.py
@@ -1,2 +1,9 @@
-rootPc = "C:/Users/user/"
-rootProject = " C:/Users/user/Desktop/Projetos/desktop_bot_py/"
+import os
+
+# Pasta do usuário atual do sistema
+rootPc = os.path.expanduser('~').replace('\\', '/')
+# Pasta atual do projeto
+rootProject = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+
+# Usuário do sistema
+user = os.getlogin()
