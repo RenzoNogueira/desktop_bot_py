e3d9947378aeac0d593551740664968730236903
RenzoNogueira <mainrenzossn@gmail.com>
2022-06-02 22:27:49 -0300

    cooreção de variáveis

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
 import os
 
 # Pasta do usuário atual do sistema
-rootPc = os.path.expanduser('~').replace('\\', '/')
+rootPc = os.path.expanduser('~').replace('\\', '/')+'/'
 # Pasta atual do projeto
-rootProject = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+rootProject = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')+'/'
 # Usuário do sistema
 user = os.getlogin()
